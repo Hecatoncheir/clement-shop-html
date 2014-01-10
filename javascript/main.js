19103d4f8f185b8bd6bb7c09eff7208e566624ec
Vitaliy <rasart.pro@gmail.com>
2014-01-10 10:04:18 +0400

    html for shop

---
+++ javascript/main.js
@@ -0,0 +1,25 @@
+
+$('.parallax').scrolly();
+$('.parallax_bg').scrolly({bgParallax: true});
+
+$(".fb").fancybox({
+        'openEffect'	:	'elastic',
+		'closeEffect'	:	'elastic',
+		'openSpeed'		:	160, 
+		'closeSpeed'	:	200, 
+});
+
+// jQuery("body").fitText(1.2, { minFontSize: '20px', maxFontSize: '20px' })
+
+// $(function() {
+//      var ewall = new freewall(".items");
+// 			ewall.reset({
+// 				selector: '.item',
+// 				animate: true,
+// 				cellW: 220,
+// 				cellH: 'auto',
+// 				onResize: function() {
+// 					ewall.fitWidth();
+// 				}
+// 			});
+// });
